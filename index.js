311e362f7982abf916fd1b3038475226fe000238
Pedro Gomes <pedrogomes94@gmail.com>
2018-12-01 16:35:00 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// Routes
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+// Listen
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server listening on port '+ port);
